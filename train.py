7e5be0124c870ee27e29b12bb52cf885990c5905
true-source <christeejacobs@gmail.com>
2018-09-27 03:41:22 +0530

    minor

--- train.py
+++ train.py
@@ -1,29 +1,25 @@
 import tensorflow as tf
 from matplotlib import pyplot as plt
 import numpy as np
+import cv2		
 from network.darknet21 import Arch
 import config.parameters as p
 from loss import loss
-import cv2
-from utils import datamaker			
 from data.loader import Loader
 
-
+dataset_path = "/home/christie/projects/hobby/yolo-tf/dataset/"
 config = p.getParams()
-loader = Loader("/home/christie/projects/hobby/yolo-tf/dataset/", config, "bbox")
+loader = Loader(dataset_path, config, "bbox")
 
-for _ in range(8):
-	images, labels = loader.next_batch(2)
+arch = Arch(config)
+preds = arch.darknet()
+x = arch.getX()
 
-# arch = Arch(config)
-
-# preds = arch.darknet()
-# x = arch.getX()
-
-# with tf.Session() as sess:
-# 	loss = loss.yolo_loss(preds, config, labels)
-# 	train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-# 	sess.run(tf.global_variables_initializer())
-# 	sess.run(train_step, feed_dict={x:images})
+with tf.Session() as sess:
+	for _ in len(open(dataset_path+"train.txt","r").readlines())/config["BATCH_SIZE"]
+		loss = loss.yolo_loss(preds, config, labels)
+		train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+		sess.run(tf.global_variables_initializer())
+		sess.run(train_step, feed_dict={x:images})
 
 
