6079d3bc39df4d6dc9f67ea2f1a821274407df34
true-source <christeejacobs@gmail.com>
2018-10-22 11:49:07 +0530

    dev test

--- train.py
+++ train.py
@@ -15,8 +15,12 @@ loader = Loader(dataset_path, config, "bbox")
 
 arch = Arch(config)
 preds = arch.darknet()
+labels = tf.placeholder(tf.float32,[None,13,13,5,6])
 x = arch.getX()
 saver = tf.train.Saver()
+
+ls = losses.yolo_loss(preds, config, labels)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(ls)
 
 if not os.path.exists(config["MODEL_SAVE_PATH"]):
 		os.mkdir(config["MODEL_SAVE_PATH"])
@@ -25,13 +29,11 @@ with tf.Session() as sess:
 	for i in range(config["EPOCH_SIZE"]):
 		for j in range(int(len(open(dataset_path+"train.txt","r").readlines())/config["BATCH_SIZE"])):			
 			print ("doing stuff on {}th batch".format(j))
-			images,labels = loader.next_batch(config["BATCH_SIZE"])
-			ls = losses.yolo_loss(preds, config, labels)
-			train_step = tf.train.AdamOptimizer(1e-4).minimize(ls)
+			images,labels_ = loader.next_batch(config["BATCH_SIZE"])
 			sess.run(tf.global_variables_initializer())
-			sess.run(train_step, feed_dict={x:images})
-			ls_val = sess.run(ls, feed_dict={x:images})
-			print ("loss : {}".format(ls_val))
+			sess.run(train_step, feed_dict={x:images,labels=labels_})
+			# ls_val = sess.run(ls, feed_dict={x:images})
+			# print ("loss : {}".format(ls_val))
 		if i%100 == 0:
 			save_path = saver.save(sess, config["MODEL_SAVE_PATH"]+"model_{}.ckpt".format(i))
 			print ("Model at {} epoch saved at {}".format(i, save_path))
