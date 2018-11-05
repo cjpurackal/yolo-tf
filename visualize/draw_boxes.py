a52e22433f0db6cca0ad43d28119769e0ac971c6
true-source <christeejacobs@gmail.com>
2018-11-05 14:48:04 +0530

    test

--- visualize/draw_boxes.py
+++ visualize/draw_boxes.py
@@ -7,7 +7,7 @@ import data.utils as utils
 def draw_box(t, img):
 	if t is None:
 		raise ValueError("Tensor cannot be None")
-	if img in None:
+	if img is None:
 		raise ValueError("Image cannot be None")
 	if exists(img):
 		raise ValueError("%s , couldn't find file"%img)
