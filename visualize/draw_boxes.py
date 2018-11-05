3ae8c053238fcd103cc3e098f16fe9f099e7108f
true-source <christeejacobs@gmail.com>
2018-11-05 14:49:27 +0530

    test

--- visualize/draw_boxes.py
+++ visualize/draw_boxes.py
@@ -7,11 +7,11 @@ import data.utils as utils
 def draw_box(t, img):
 	if t is None:
 		raise ValueError("Tensor cannot be None")
-	if img is None:
-		raise ValueError("Image cannot be None")
-	if exists(img):
-		raise ValueError("%s , couldn't find file"%img)
-	img = plt.imread(img)
+	# if img is None:
+	# 	raise ValueError("Image cannot be None")
+	# if exists(img):
+	# 	raise ValueError("%s , couldn't find file"%img)
+	# img = plt.imread(img)
 	where = tf.not_equal(t[0,:,:,:,0],0)
 	indices = tf.where(where)
 	print (indices)
