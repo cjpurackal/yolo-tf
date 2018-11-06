a4dd9ab14cd77b898b5df52247b8444cd5eb0fa3
true-source <christeejacobs@gmail.com>
2018-11-06 17:54:30 +0530

    minor

--- visualize/draw_boxes.py
+++ visualize/draw_boxes.py
@@ -5,7 +5,7 @@ import data.utils as utils
 import tensorflow as tf
 
 
-def draw_box(t, img):
+def draw_boxes(t, img, config):
 	if t is None:
 		raise ValueError("Tensor cannot be None")
 	# if img is None:
@@ -15,4 +15,17 @@ def draw_box(t, img):
 	# img = plt.imread(img)
 	where = tf.not_equal(t[0,:,:,:,0],0)
 	indices = tf.where(where)
-	print (indices)
+	boxes = []
+	for grid_x, grid_y, box_prior in indices:
+		cx, cy, w, h = t[0, grid_x, grid_y, box_prior, 0:4]
+		cx = cx * (self.config["GRID_W"]/self.config["IMAGE_W"])
+		cy = cy * (self.config["GRID_H"]/self.config["IMAGE_H"])
+		w = w * (self.config["GRID_W"]/self.config["IMAGE_W"])
+		h = h * (self.config["GRID_H"]/self.config["IMAGE_H"])
+		x0 = cx - w/2
+		y0 = cy - h/2
+		box = [x0, y0, w, h]
+		boxes.append(box)
+
+	print ("found %d boxes" % len(boxes))
+	print (boxes)
