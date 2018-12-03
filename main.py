34ff012c7e852a29fd25e45642c88fbf318dedb1
true-source <christeejacobs@gmail.com>
2018-12-03 17:37:04 +0530

    name bug

--- main.py
+++ main.py
@@ -61,14 +61,16 @@ with tf.Session() as sess:
 		print (type(img))
 		img = img.reshape([1, config["IMAGE_H"], config["IMAGE_W"], 3])
 		print (img.shape)
-		inp = tf.get_default_graph().get_operation_by_name('input').outputs
-		# out = tf.get_default_graph().get_tensor_by_name('predictions:0').outputs
+		inp = tf.get_default_graph().get_operation_by_name('input').outputs[0]
+		out = tf.get_default_graph().get_tensor_by_name('predictions').outputs[0]
+		print (inp.shape)
+		print (out.shape)
 		# for v in tf.get_default_graph().get_collection("variables"):
 		# 	print (v)
 		# 	input()
-		for x in tf.get_default_graph().get_operations():
-			if x.name == "input":
-				print(x)		
+		# for x in tf.get_default_graph().get_operations():
+		# 	if x.name == "input":
+		# 		print(x)		
 		# input()
 		# for v in tf.get_default_graph().get_collection("trainable_variables"):
 		# 	print (v)
