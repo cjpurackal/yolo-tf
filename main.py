1c445592d666779863919e8b9d2b7b09a575cf8f
true-source <christeejacobs@gmail.com>
2018-12-03 15:53:10 +0530

    name bug

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ with tf.Session() as sess:
 				save_path = saver.save(sess, config["MODEL_SAVE_PATH"]+"model_{}.ckpt".format(i))
 				print ("Model at {} epoch saved at {}".format(i, save_path))
 	elif sys.argv[1] == "test":
-		saver = tf.train.import_meta_graph(config["MODEL_SAVE_PATH"]+"model_900.ckpt.meta")
+		saver = tf.train.import_meta_graph(config["MODEL_SAVE_PATH"]+"model_100.ckpt.meta")
 		saver.restore(sess, config["MODEL_SAVE_PATH"]+"model_900.ckpt")
 		print ("model restored!")
 		img = data.utils.manip_image(sys.argv[2], config)
