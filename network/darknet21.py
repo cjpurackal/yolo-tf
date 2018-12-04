a1d5cf433f6a2362a036dc1efec654dd36c68abb
true-source <christeejacobs@gmail.com>
2018-12-04 19:41:30 +0530

    tf_cnnvis

--- network/darknet21.py
+++ network/darknet21.py
@@ -7,6 +7,7 @@ class Arch:
 	def __init__(self,config):
 		self.config = config
 		self.x = tf.placeholder(dtype=tf.float32,shape=[None, config["IMAGE_W"], config["IMAGE_H"], 3], name="input")
+		tf.add_to_collection("input", self.x)
 
 	def darknet(self):
 		conv1 = tf.layers.conv2d(self.x, filters=32, kernel_size=[3, 3], padding="same", activation=tf.nn.leaky_relu)
@@ -60,6 +61,7 @@ class Arch:
 		conv21 = tf.layers.batch_normalization(conv21)
 		predictions = tf.layers.conv2d(conv21, filters=self.config["BOX"] * (4 + 1 + self.config["CLASS"]), kernel_size=[1, 1], padding="same", activation=tf.nn.relu)	
 		predictions = tf.reshape(predictions, [-1, self.config["GRID_H"], self.config["GRID_W"], self.config["BOX"], 4 + 1 + self.config["CLASS"]], name="predictions")
+		tf.add_to_collection("predictions", predictions)
 		return predictions
 
 
