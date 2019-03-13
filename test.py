00a96052f973a6035e9ea1da4f1f59762a0a5c1c
true-source <christeejacobs@gmail.com>
2019-03-13 16:56:19 +0530

    updated test

--- test.py
+++ test.py
@@ -1,15 +1,25 @@
-import tensorflow as tf
-# tf.enable_eager_execution()
-
-from matplotlib import pyplot as plt
-import numpy as np
-import cv2
 import sys
-import os
-from network.darknet21 import Arch
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.layers import Input, Conv2D, Lambda, Reshape
+from models import Darknet21
+from loss import yolo_loss
 import config.parameters as p
-# from loss import losses
 import data
 from data.loader import Loader
-from visualize import draw
-import tf_cnnvis
+
+save_path = "/tmp/yolo-tf/"
+saved_weights_path = sys.argv[1]
+test_img = sys.argv[2]
+
+config = p.getParams()
+
+
+with tf.Session() as sess:
+	saver = tf.train.import_meta_graph(saved_weights_path)
+	checkpoint = tf.train.latest_checkpoint(save_path)
+	saver.restore(sess, checkpoint)
+	# img = data.utils.manip_image(test_img, config)
+	# img = img.reshape([1, config["IMAGE_H"], config["IMAGE_W"], 3])
+	# inp = tf.get_default_graph().get_tensor_by_name("input:0")
+	# out = tf.get_default_graph().get_tensor_by_name("predictions:0")
